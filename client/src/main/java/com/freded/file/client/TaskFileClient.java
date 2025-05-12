775094c63e4de41696138d92c4e2f5f69419e457
Fredrick Okoro <fredrick.okoro@tech11.com>
2025-05-12 20:35:56 +0200

    chore: extend client

--- client/src/main/java/com/freded/file/client/TaskFileClient.java
+++ client/src/main/java/com/freded/file/client/TaskFileClient.java
@@ -2,22 +2,30 @@ package com.freded.file.client;
 
 
 import com.freded.dtos.TaskFileDTO;
-import com.freded.file.client.boundary.TaskFile;
+import com.freded.file.client.entity.TaskFileSortAndPaginationDTO;
 import com.freded.file.client.entity.TaskFileUploadDTO;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
+import java.util.List;
+
+@ApplicationScoped
 public class TaskFileClient {
 
     @Inject
     @RestClient
-    TaskFile restClient;
+    TaskFileRestClient taskFileRestClient;
 
-    public TaskFileDTO save(final String taskId, TaskFileUploadDTO form) {
-        return restClient.uploadFileToTask(taskId, form);
+    public TaskFileDTO uploadFileToTask(final String taskId, final TaskFileUploadDTO form) {
+        return taskFileRestClient.uploadFileToTask(taskId, form);
     }
 
-    public TaskFileDTO get(final String taskFileId) {
-        return restClient.getFileDetails(taskFileId);
+    public TaskFileDTO getFileDetails(final String taskFileId) {
+        return taskFileRestClient.getFileDetails(taskFileId);
+    }
+
+    public List<TaskFileDTO> getFilesForTask(final String taskId, final TaskFileSortAndPaginationDTO qParams) {
+        return taskFileRestClient.getFilesForTask(taskId, qParams);
     }
 }
